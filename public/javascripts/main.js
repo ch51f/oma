309659da5da0cf16bc51fd1031fa0d78a18848d5
ch51ff <371476909@qq.com>
2016-05-23 14:53:11 +0800

    添加favicon,nav交互

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 $(function() {
+	var $nav = $("#nav");
 	$("#contact-form").Validform({
 		ajaxPost: true,
 		callback: function(res) {
@@ -11,4 +12,35 @@ $(function() {
 			}
 		}
 	});
+
+	var arr = ['b', 'd', 'e'];
+	var arrTop = [];
+
+	for(var i = 0; i < arr.length; i++) {
+		var temp = $("#panel-" + arr[i]);
+		arrTop.push(temp.offset().top);
+	}
+
+	$(window).scroll(function() {
+		var temp = $("body").scrollTop();
+		if(temp < arrTop[0]) {
+			navSelect(0);
+		} else if (temp >= arrTop[0] && temp < arrTop[1]) {
+			navSelect(1);
+		} else if (temp >= arrTop[1] && temp < arrTop[2]) {
+			navSelect(2);
+		} else {
+			navSelect(3);
+		}
+	})
+
+	function navSelect(index) {
+		$nav.find("a").each(function(i, e) {
+			if(i == index) {
+				$(e).addClass("active");
+			} else {
+				$(e).removeClass("active");
+			}
+		});
+	}
 });
