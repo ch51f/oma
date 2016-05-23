050d77d228b37afcac2ac2a77624e4e39f9a0332
ch51ff <371476909@qq.com>
2016-05-23 17:00:27 +0800

    lab begin

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ var contactDao = require('../dao/contactDao.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('page/index', { title: 'OhMyApps' });
+	res.render('page/index', { title: 'OhMyApps' });
 });
 
 router.post('/addContact', function(req, res, next) {
 	contactDao.add(req, res, next);
 });
+
+router.get('/lab', function(req, res, next) {
+	res.render('page/lab', {title: 'OMA Lab'})
+})
 
 // router.get('/omaquery', function(req, res, next) {
 // 	contactDao.queryAll(req, res, next);
